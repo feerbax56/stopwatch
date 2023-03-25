a30a846f3d9c4b9ef5b54416f1014d41cf45c017
Evseev Anton <soki1997@mail.ru>
2023-03-25 15:39:06 +0500

    add: counter css

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <CounterStopwatch/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import CounterStopwatch from './components/Counter.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CounterStopwatch
   }
 }
 </script>
---
+++ src/components/Counter.vue
@@ -0,0 +1,25 @@
+<template>
+
+
+  <div class="stopwatch">
+    xxx
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'CounterStopwatch',
+
+}
+</script>
+
+<style>
+.stopwatch{
+  width: 255px;
+  height: 120px;
+  background-color: #696969;
+  color: #9E9E9E;
+}
+
+</style>
